09e4dd2c07726d14285c42e905d4bda18c5c4cd0
caleb <duocaleb3@gmail.com>
2024-06-07 14:05:01 -0400

    Submit

--- Main.java
+++ Main.java
@@ -13,4 +13,5 @@ class Main {
 	  PApplet.runSketch(processingArgs, mySketch);
   }
   
+  
 }
--- Sketch.java
+++ Sketch.java
@@ -1,36 +1,82 @@
 import processing.core.PApplet;
+import processing.core.PImage;
 
 public class Sketch extends PApplet {
 	
-	
-  /**
-   * Called once at the beginning of execution, put your size all in this method
-   */
+
+  PImage imgBackground;
+  PImage imgCircle;
+  boolean boolCnBounceVert = false;
+  boolean boolCnBounceHorz = false;
+  boolean boolDoneVert = false;
+
+  // All variables from here on are editable!
+  int[] intCircleSize = {30,30};
+  int intScreenSize = 800;
+  double[] dblCircleLoc = {intScreenSize/2-intCircleSize[0]/2,0};
+  double[] dblCircleVel = {30,15};
+  double dblGravity = 0.2;
+  double dblBouncines = 0.5;
+  double dblFloorFriction = 0.99;
+  
   public void settings() {
 	// put your size call here
-    size(400, 400);
+    size(intScreenSize, intScreenSize);
   }
-
-  /** 
-   * Called once at the beginning of execution.  Add initial set up
-   * values here i.e background, stroke, fill etc.
-   */
   public void setup() {
     background(210, 255, 173);
+    imgCircle = loadImage("pngimg.com - circle_PNG50.png");
+    imgCircle.resize(imgCircle.width/imgCircle.width*intCircleSize[0],imgCircle.height/imgCircle.height*intCircleSize[1]);
+    imgBackground = loadImage("pexels-pixabay-531880.png");
+    imgBackground.resize(imgBackground.width/imgBackground.width*intScreenSize,imgBackground.height/imgBackground.height*intScreenSize);
   }
 
   /**
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
+    image(imgBackground, 0, 0);
+    // Falling
+    if(dblCircleLoc[1] < intScreenSize-imgCircle.height && boolDoneVert == false){
+      dblCircleVel[1] += dblGravity;
+      boolCnBounceVert = true;
+    }
+    // Verticle bouncing top
+    if(dblCircleLoc[1] < 0 && boolCnBounceVert){
+      boolCnBounceVert = false;
+      dblCircleVel[1] *= -dblBouncines;
+    }
+    // Verticle bouncing bottom
+    if(dblCircleLoc[1] >= intScreenSize-imgCircle.height && boolCnBounceVert){
+      boolCnBounceVert = false;
+      dblCircleVel[1] *= -dblBouncines;
+      if(dblCircleVel[1] < 1.1 && dblCircleVel[1] > -1.1){
+        dblCircleVel[1] = 0;
+        boolDoneVert = true;
+      }
+    }
+    // Horizontal bouncing
+    if((dblCircleLoc[0] >= intScreenSize-imgCircle.width || dblCircleLoc[0] <= 0) && boolCnBounceHorz){
+      dblCircleVel[0] *= -dblBouncines;
+      boolCnBounceHorz = false;
+    }
+    else{
+      boolCnBounceHorz = true;
+    }
 
-    stroke(255);
-    line(50, 125, 70, 50);  
-  }
-  
-  // define other methods down here.
+    // Friction
+    if(dblCircleLoc[1] >= intScreenSize-imgCircle.height){
+      dblCircleVel[0] *= dblFloorFriction;
+      if(dblCircleVel[0] < 0.17 && dblCircleVel[0] > -0.17){
+        dblCircleVel[0] = 0;
+      }
+    }
+
+    // Adding
+    dblCircleLoc[0] += dblCircleVel[0];
+    dblCircleLoc[1] += dblCircleVel[1];
+
+    // Putting on screen
+    image(imgCircle,(int)dblCircleLoc[0],(int)dblCircleLoc[1]);
+  }  
 }
